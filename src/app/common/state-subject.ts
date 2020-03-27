74c208d20c024bdc6197302558fcbc56be873fac
mico <mi-const@yandex.ru>
2020-03-27 20:09:20 +0300

    + state keys

--- src/app/common/state-subject.ts
+++ src/app/common/state-subject.ts
@@ -3,6 +3,7 @@ import { BehaviorSubject, Observable } from 'rxjs';
 
 export class StateSubject<T> {
   protected _stateSubject: BehaviorSubject<Readonly<T>>;
+  protected _stateKeys: Readonly<string[]>;
 
   get state(): Observable<Readonly<T>>  {
     return this._stateSubject.asObservable();
@@ -12,9 +13,18 @@ export class StateSubject<T> {
     return this._stateSubject.value;
   }
 
+  get keys(): Readonly<string[]> {
+    return this._stateKeys;
+  }
+
   constructor(initialValue: Readonly<T> | null = null) {
     this._stateSubject = new BehaviorSubject(initialValue);
+    this._stateKeys = Object.keys(initialValue);
   }
+
+  // getItem(key: keyof T) {
+  //   return this.value[key];
+  // }
 
   /**
    * Overwrite this function to validate the new state. Default implementation just returns the unaltered state object.
@@ -26,9 +36,20 @@ export class StateSubject<T> {
 
   protected _next(params: Partial<Readonly<T>>): boolean {
     const state = this._validate({ ...this.value, ...params });
-    if (state) {
+    if (state && !this.equials(state)) {
       this._stateSubject.next(state);
+      return true;
     }
-    return !!state;
+    return false;
+  }
+
+  equials(newState: Readonly<T>): boolean {
+    const oldState = this.value;
+    for (const key of this.keys) {
+      if (newState[key] !== oldState[key]) {
+        return false;
+      }
+    }
+    return true;
   }
 }
