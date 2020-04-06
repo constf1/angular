660d1abddd3108f959a8ed5756a5c4fc4a6dc281
mico <mi-const@yandex.ru>
2020-04-06 14:33:31 +0300

    cleaning

--- src/app/common/pipes/xrange.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { XrangePipe } from './xrange.pipe';
-
-describe('XrangePipe', () => {
-  it('create an instance', () => {
-    const pipe = new XrangePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
