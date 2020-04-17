bd77889840a16a2f77c53da462c5804c957d602a
mico <mi-const@yandex.ru>
2020-04-17 12:31:50 +0300

    at start navigate to a random game

--- src/app/freecell/freecell-demo/freecell-demo.component.ts
+++ src/app/freecell/freecell-demo/freecell-demo.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 import { PROJECT_NAME, VERSION } from 'src/environments/constants';
-
-
 
 @Component({
   selector: 'app-freecell-demo',
@@ -11,9 +10,13 @@ import { PROJECT_NAME, VERSION } from 'src/environments/constants';
 export class FreecellDemoComponent implements OnInit {
   readonly info = `${PROJECT_NAME} ver.${VERSION}`;
 
-  constructor() { }
+  constructor(router: Router, route: ActivatedRoute) {
+    const params = route.snapshot.queryParams;
+    if (params.deal === undefined || isNaN(parseInt(params.deal, 10))) {
+      router.navigate(['.'], { relativeTo: route, queryParams: { deal: Date.now() % 0x80000000 }});
+    }
+  }
 
   ngOnInit(): void {
   }
-
 }
