e3a70d46edb74f6ef6a46a0e4da0cb4a078935f6
mico <mi-const@yandex.ru>
2020-06-26 15:00:27 +0300

    + focus directive

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,10 +4,19 @@ import { CommonModule } from '@angular/common';
 import { GestureDirective } from './directives/gesture.directive';
 
 import { XrangePipe } from './pipes/xrange.pipe';
+import { FocusDirective } from './directives/focus.directive';
 
 @NgModule({
-  declarations: [GestureDirective, XrangePipe],
-  exports: [GestureDirective, XrangePipe],
+  declarations: [
+    FocusDirective,
+    GestureDirective,
+    XrangePipe,
+  ],
+  exports: [
+    FocusDirective,
+    GestureDirective,
+    XrangePipe,
+  ],
   imports: [
     CommonModule
   ]
---
+++ src/app/core/directives/focus.directive.ts
@@ -0,0 +1,17 @@
+// tslint:disable: variable-name
+import { Directive, ElementRef, AfterViewInit } from '@angular/core';
+
+@Directive({
+  selector: '[appFocus]'
+})
+export class FocusDirective implements AfterViewInit {
+  constructor(private _ref: ElementRef/*, private _renderer: Renderer2*/) { }
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      const elem = this._ref?.nativeElement;
+      if (elem) {
+        elem.focus();
+      }
+    });
+  }
+}
