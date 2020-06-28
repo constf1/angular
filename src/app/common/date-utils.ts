16d37a33cf58525bf8cbafac198487043f62e988
mico <mi-const@yandex.ru>
2020-06-28 13:03:45 +0300

    + various date formats

--- src/app/common/date-utils.ts
+++ src/app/common/date-utils.ts
@@ -4,10 +4,13 @@
 
 import { toString } from './math-utils';
 
+export const DATE_FORMATS = ['YYYY-MM-DD HH:mm:ss', 'YYYY-MM-DD HH:mm', 'YYYY-MM-DD'] as const;
+export type DateFormat = typeof DATE_FORMATS[number];
+
 /**
  * Returns current date in 'YYYY-MM-DD HH:mm:ss' format.
  */
-export function getDate(): string {
+export function getDate(dateFormat?: DateFormat): string {
   const now = new Date();
 
   const Y = now.getFullYear();
@@ -17,6 +20,12 @@ export function getDate(): string {
   const H = toString(now.getHours(), 2);
   const m = toString(now.getMinutes(), 2);
   const s = toString(now.getSeconds(), 2);
+
+  switch (dateFormat) {
+    case 'YYYY-MM-DD HH:mm:ss': return `${Y}-${M}-${D} ${H}:${m}:${s}`;
+    case 'YYYY-MM-DD HH:mm': return `${Y}-${M}-${D} ${H}:${m}`;
+    case 'YYYY-MM-DD': return `${Y}-${M}-${D}`;
+  }
 
   return `${Y}-${M}-${D} ${H}:${m}:${s}`;
 }
