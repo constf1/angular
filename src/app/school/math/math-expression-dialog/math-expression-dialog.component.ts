efcf8ccbdec33f93f686e037a090a50bee12178a
mico <mi-const@yandex.ru>
2020-07-09 14:27:40 +0300

    + multiplication

--- src/app/school/math/math-expression-dialog/math-expression-dialog.component.ts
+++ src/app/school/math/math-expression-dialog/math-expression-dialog.component.ts
@@ -2,6 +2,15 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 import { InputItem } from '../math-input-group/math-input-group.component';
+import {
+  getAdditionWithZeroRule,
+  getAdditionRule,
+  getSubtractionWithZeroRule,
+  getSubtractionRule,
+  getMultiplicationRule,
+  getDivisionRule
+} from '../simple-math-rules';
+import { asString } from '../math-models';
 
 @Component({
   selector: 'app-math-expression-dialog',
@@ -11,124 +20,77 @@ import { InputItem } from '../math-input-group/math-input-group.component';
 export class MathExpressionDialogComponent implements OnInit {
   title = '';
   content = '';
-  augend = 0;
-  addend = 0;
+  contentType: 'addition' | 'multiplication' | null = null;
+  firstTermValue = 0;
+  secondTermValue = 0;
   hiddenTerm = -1;
 
   constructor(@Inject(MAT_DIALOG_DATA) public item: InputItem) { }
 
   ngOnInit(): void {
-    const name = this.item.expression.name;
-    if (name === 'addition') {
-      this._initAddition();
-      const zeroRule = this._getAdditionWithZeroRule();
+    const term = this.item.inputIndex;
+    const expr = this.item.expression;
+
+    this.title = asString(expr, term);
+
+    if (expr.name === 'addition') {
+      this.firstTermValue = expr.first.value;
+      this.secondTermValue = expr.second.value;
+      this.content = getAdditionRule(expr, term);
+      this.contentType = 'addition';
+
+      const zeroRule = getAdditionWithZeroRule(expr, term);
       if (zeroRule) {
         this.content = zeroRule;
         this.hiddenTerm = -1;
+      } else if (term === 'first') {
+        this.hiddenTerm = 0;
+      } else if (term === 'second') {
+        this.hiddenTerm = 1;
+      } else {
+        this.hiddenTerm = 2;
       }
-    } else if (name === 'subtraction') {
-      this._initSubtraction();
-      const zeroRule = this._getSubtractionWithZeroRule();
+    } else if (expr.name === 'subtraction') {
+      this.firstTermValue = expr.result.value;
+      this.secondTermValue = expr.second.value;
+      this.contentType = 'addition';
+      this.content = getSubtractionRule(expr, term);
+
+      const zeroRule = getSubtractionWithZeroRule(expr, term);
       if (zeroRule) {
         this.content = zeroRule;
         this.hiddenTerm = -1;
-      }
-    }
-  }
-
-  private _getAdditionWithZeroRule(): string | null {
-    const { first, second, result } = this.item.expression;
-    const isFirstZero = second.value === result.value;
-    const isSecondZero = first.value === result.value;
-    const isResultZero = isFirstZero && isSecondZero;
-
-    // 0 + 0 = 0
-    if (isResultZero) {
-      return 'Сколько ноль не прибавляй, всё равно ничего не изменится!';
-    }
-
-    const inputIndex = this.item.inputIndex;
-    // ? + x = x or x + ? = x
-    if ((isFirstZero && inputIndex === 'first') || (isSecondZero && inputIndex === 'second')) {
-      return `Какое число нужно прибавить к числу ${result.value} чтобы оно не изменилось?`;
-    }
-    // 0 + x = ? or 0 + ? = x
-    if (isFirstZero && inputIndex !== 'first') {
-      return 'Если к нулю прибавить число, то получится то же самое число.';
-    }
-    // x + 0 = ? or ? + 0 = x
-    if (isSecondZero && inputIndex !== 'second') {
-      return 'Если к любому числу прибавить 0 то это всё равно, что ничего к нему не прибавить!';
-    }
-    return null;
-  }
-
-  private _getSubtractionWithZeroRule(): string | null {
-    const { first, second, result } = this.item.expression;
-    const isResultZero = first.value === second.value;
-    const isFirstZero = first.value === 0;
-    const isSecondZero = second.value === 0;
-    const inputIndex = this.item.inputIndex;
-
-    // x - x = 0
-    if (isResultZero) {
-      if (isFirstZero) {
-        return 'Сколько ноль не отнимай, всё равно ничего не изменится!';
+      } else if (term === 'first') {
+        this.hiddenTerm = 2;
+      } else if (term === 'second') {
+        this.hiddenTerm = 1;
       } else {
-        return 'Разность двух одинаковых чисел всегда равна нулю.';
+        this.hiddenTerm = 0;
       }
-    }
-
-    if (isSecondZero) {
-      // x - ? = x
-      if (inputIndex === 'second') {
-        return `Какое число нужно отнять от числа ${result.value} чтобы оно не изменилось?`;
+    } else if (expr.name === 'multiplication') {
+      this.firstTermValue = expr.first.value;
+      this.secondTermValue = expr.second.value;
+      this.contentType = 'multiplication';
+      this.content = getMultiplicationRule(expr, term);
+      if (term === 'first') {
+        this.hiddenTerm = 0;
+      } else if (term === 'second') {
+        this.hiddenTerm = 1;
       } else {
-        return 'При вычитании нуля из любого числа всегда получается это самое число!';
+        this.hiddenTerm = 2;
       }
-    }
-
-    return null;
-  }
-
-  private _initSubtraction() {
-    const item = this.item;
-    const expr = item.expression;
-    this.augend = expr.result.value;
-    this.addend = expr.second.value;
-    if (item.inputIndex === 'first') {
-      this.hiddenTerm = 2;
-      this.title = `? - ${expr.second.value} = ${expr.result.value}`;
-      this.content = 'Чтобы найти уменьшаемое (?), надо сложить разность и вычитаемое.';
-    } else if (item.inputIndex === 'second') {
-      this.hiddenTerm = 1;
-      this.title = `${expr.first.value} - ? = ${expr.result.value}`;
-      this.content = 'Чтобы найти вычитаемое (?), надо из уменьшаемого вычесть разность.';
-    } else {
-      this.hiddenTerm = 0;
-      this.title = `${expr.first.value} - ${expr.second.value} = ?`;
-      this.content = 'Чтобы найти разность (?), надо из уменьшаемого вычесть вычитаемое.';
-    }
-  }
-
-  private _initAddition() {
-    const { first, second, result } = this.item.expression;
-    this.augend = first.value;
-    this.addend = second.value;
-
-    const inputIndex = this.item.inputIndex;
-    if (inputIndex === 'first') {
-      this.title = `? + ${second.value} = ${result.value}`;
-      this.hiddenTerm = 0;
-      this.content = 'Чтобы найти первое слагаемое (?), надо из суммы вычесть второе слагаемое.';
-    } else if (inputIndex === 'second') {
-      this.title = `${first.value} + ? = ${result.value}`;
-      this.hiddenTerm = 1;
-      this.content = 'Чтобы найти второе слагаемое (?), надо из суммы вычесть первое слагаемое.';
-    } else {
-      this.title = `${first.value} + ${second.value} = ?`;
-      this.hiddenTerm = 2;
-      this.content = 'Чтобы найти сумму (?), надо сложить первое и второе слагаемые.';
+    } else if (expr.name === 'division') {
+      this.firstTermValue = expr.result.value;
+      this.secondTermValue = expr.second.value;
+      this.contentType = 'multiplication';
+      this.content = getDivisionRule(expr, term);
+      if (term === 'first') {
+        this.hiddenTerm = 2;
+      } else if (term === 'second') {
+        this.hiddenTerm = 1;
+      } else {
+        this.hiddenTerm = 0;
+      }
     }
   }
 }
