51da74184e86c1e499fd6ec4064afdffb24575d7
mico <mi-const@yandex.ru>
2020-10-11 16:54:16 +0300

    + path data history service

--- src/app/svg-path-editor/services/path-data.service.ts
+++ src/app/svg-path-editor/services/path-data.service.ts
@@ -1,13 +1,109 @@
 // tslint:disable: variable-name
 
 import { Injectable } from '@angular/core';
+import { StateSubject } from 'src/app/common/state-subject';
 
-import { PathModel } from '../path-model';
+export interface PathDataState {
+  data: string;
+  prev?: Readonly<PathDataState>;
+  next?: Readonly<PathDataState>;
+}
+
+export const initialState: Readonly<PathDataState> = {
+  data: '',
+  prev: null,
+  next: null
+};
+
+const KEY = '[svg-path-editor.path-data]';
 
 @Injectable()
-export class PathDataService {
-  private _pathDataInput = '';
-  private _pathModel = new PathModel();
+export class PathDataService extends StateSubject<PathDataState> {
+  private _timerId: ReturnType<typeof setTimeout>;
 
-  constructor() { }
+  /** Save delay in milliseconds */
+  saveDelay = 30 * 1000;
+
+  get pathData() {
+    return this.state.data;
+  }
+
+  set pathData(value: string) {
+    const state = this.state;
+    if (state.data !== value) {
+      this._set({ data: value, prev: state, next: null });
+    }
+  }
+
+  get canUndo(): boolean {
+    return !!this.state.prev;
+  }
+
+  get canRedo(): boolean {
+    return !!this.state.next;
+  }
+
+  get hasLink(): boolean {
+    return !(!this.state.prev && !this.state.next);
+  }
+
+  private _save = () => {
+    this._timerId = null;
+    try {
+      localStorage.setItem(KEY, this.state.data);
+    } catch (e) {
+      console.warn('localStorage error:', e);
+    }
+  }
+
+  constructor() {
+    super(initialState);
+
+    try {
+      const data = localStorage?.getItem(KEY);
+      if (data) {
+        this._set({ data });
+      }
+    } catch (e) {
+      console.warn('localStorage error:', e);
+    }
+  }
+
+  restoreDefaults() {
+    this._set(initialState);
+  }
+
+  undo() {
+    const state = this.state;
+    const prev = state.prev;
+    if (prev) {
+      this._set({ data: prev.data, prev: prev.prev, next: state });
+    }
+  }
+
+  redo() {
+    const state = this.state;
+    const next = state.next;
+    if (next) {
+      this._set({ data: next.data, prev: state, next: next.next });
+    }
+  }
+
+  unlink() {
+    const state = this.state;
+    if (state.next || state.prev) {
+      this._set({ data: state.data, prev: null, next: null });
+    }
+  }
+
+  protected _set(params: Partial<Readonly<PathDataState>>): boolean {
+    const ok = super._set(params);
+    if (ok) {
+      if (!this._timerId) {
+        this._timerId = setTimeout(this._save, this.saveDelay);
+      }
+      // console.log('PATH:' + this.state.data.substring(0, 20) + '...');
+    }
+    return ok;
+  }
 }
