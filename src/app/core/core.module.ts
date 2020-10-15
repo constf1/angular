42a30445b00cb35945dbcc44b75d232021975eb2
mico <mi-const@yandex.ru>
2020-10-15 20:14:28 +0300

    + tab-group component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { XrangePipe } from './pipes/xrange.pipe';
+
+import { FocusDirective } from './directives/focus.directive';
 import { GestureDirective } from './directives/gesture.directive';
 
-import { XrangePipe } from './pipes/xrange.pipe';
-import { FocusDirective } from './directives/focus.directive';
+import { TabGroupComponent } from './components/tab-group/tab-group.component';
 
 @NgModule({
   declarations: [
     FocusDirective,
     GestureDirective,
+    TabGroupComponent,
     XrangePipe,
   ],
   exports: [
     FocusDirective,
     GestureDirective,
+    TabGroupComponent,
     XrangePipe,
   ],
   imports: [
