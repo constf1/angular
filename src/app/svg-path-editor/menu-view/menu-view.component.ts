12733a9064114f8a3f2c77cfd5f5f758d6b30959
mico <mi-const@yandex.ru>
2020-10-18 17:29:49 +0300

    + menu-view component

---
+++ src/app/svg-path-editor/menu-view/menu-view.component.ts
@@ -0,0 +1,37 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { EditorSettingsService, FE_COLOR_MATRICES } from '../services/editor-settings.service';
+import { BackgroundImageService } from '../services/background-image.service';
+
+@Component({
+  selector: 'app-menu-view',
+  templateUrl: './menu-view.component.html',
+  styleUrls: ['./menu-view.component.scss']
+})
+export class MenuViewComponent implements OnInit {
+  @Output() requestSmallestViewBox = new EventEmitter<void>();
+
+  tabSelection = 0;
+
+  zooms: { label: string, value: number}[]  = [
+    {label: '1:4', value: 25 },
+    {label: '1:2', value: 50 },
+    {label: '1:1', value: 100 },
+    {label: '2:1', value: 200 },
+    {label: '4:1', value: 400 },
+  ];
+
+  get zoom() {
+    return this.settings.state.zoom;
+  }
+
+  set zoom(value: number) {
+    this.settings.set({ zoom: value });
+  }
+
+  imageColorMatrixNames = Object.keys(FE_COLOR_MATRICES);
+
+  constructor(public settings: EditorSettingsService, public background: BackgroundImageService) { }
+
+  ngOnInit(): void {
+  }
+}
