4dd927142bd9dc0bcd7ce37b504b1066bd733eb0
mico <mi-const@yandex.ru>
2020-10-18 17:25:29 +0300

    + tab splitter

--- src/app/core/components/tab-group/tab-group.component.ts
+++ src/app/core/components/tab-group/tab-group.component.ts
@@ -1,12 +1,22 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
+export const TAB_SPLITTER = '|';
+
 @Component({
   selector: 'app-tab-group',
   templateUrl: './tab-group.component.html',
   styleUrls: ['./tab-group.component.scss']
 })
 export class TabGroupComponent {
-  @Input() items: string[] = [];
+  labels: string[] = [];
+
+  @Input() set items(value: string[] | string) {
+    if (typeof value === 'string') {
+      this.labels = value.split(TAB_SPLITTER);
+    } else {
+      this.labels = value;
+    }
+  }
   @Input() selection = -1;
   @Output() selectionChange = new EventEmitter<number>();
 
