4893d51fd61a7c7fd4c0006882f1a6b5b16361a1
mico <mi-const@yandex.ru>
2020-10-29 10:39:13 +0300

    + formatDecimal

--- src/app/common/math-utils.ts
+++ src/app/common/math-utils.ts
@@ -26,6 +26,32 @@ export function toString(value: number, minWidth: number, prefix: string = '0', 
 }
 
 /**
+ * Returns a string representing a number in fixed-point notation.
+ * @param value The number.
+ * @param maximumFractionDigits Number of digits after the decimal point. Must be in the range 0 - 20, inclusive.
+ */
+export function formatDecimal(value: number, maximumFractionDigits?: number): string {
+  const str = value.toFixed(maximumFractionDigits);
+  if (/\.\d*$/.test(str)) {
+    let length = str.length;
+    while (true) {
+      if (str.endsWith('0', length)) {
+        length--;
+      } else {
+        if (str.endsWith('.', length)) {
+          length--;
+        }
+        break;
+      }
+    }
+    if (length < str.length) {
+      return str.substring(0, length);
+    }
+  }
+  return str;
+}
+
+/**
  * Returns a random number between minValue (inclusive) and maxValue (exclusive)
  */
 export function randomNumber(minValue: number, maxValue: number): number {
