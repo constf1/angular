af1aa9f21475f75bf85db25dad486e7bf1c754fd
mico <mi-const@yandex.ru>
2020-12-21 14:38:17 +0300

    + linear interpolator

--- src/app/common/math-utils.ts
+++ src/app/common/math-utils.ts
@@ -105,3 +105,13 @@ export function stringToByteArray(str: string): number[] {
   }
   return arr;
 }
+
+/**
+ * Linear interpolation.
+ * @param a start point
+ * @param b stop point
+ * @param t blend factor in the interval [0, 1]
+ */
+export function lerp(a: number, b: number, t: number): number {
+  return a * (1 - t) + b * t;
+}
