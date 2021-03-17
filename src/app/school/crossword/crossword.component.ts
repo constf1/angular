2d98331e350ad5234e610ff5408eca3ab5e5b0dc
mico <mi-const@yandex.ru>
2021-03-17 18:19:39 +0300

    crossword: prototype stable

--- src/app/school/crossword/crossword.component.ts
+++ src/app/school/crossword/crossword.component.ts
@@ -1,7 +1,8 @@
+// tslint:disable: variable-name
 import { Component, OnInit } from '@angular/core';
-import { Point } from 'src/app/common/math2d';
 
-import { CWItem, getItemHeight, getItemWidth } from './crossword-model';
+import { CWItem } from './crossword-model';
+import { CellState } from './crossword-board/crossword-board.component';
 
 const TEST = 'begin,0,6,0;black,0,11,0;assault,1,3,0;astronomy,1,9,0;church,1,15,0;blazing,3,22,0;assassin,4,0,0;'
   + 'abomination,7,5,0;chariots,7,17,0;apartment,9,7,0;believer,9,20,0;ball,10,14,0;blink,11,12,0;cable,12,24,0;'
@@ -24,117 +25,12 @@ function parse(puzzle: string): CWItem[] {
   return buf;
 }
 
-function createGame(items: CWItem[]) {
-  let cols = 0;
-  let rows = 0;
-  const cells = [] as CWCell[];
+// const ASSETS_URL = 'assets/school/crossword/';
 
-  for (const item of items) {
-    cols = Math.max(cols, item.x + getItemWidth(item));
-    rows = Math.max(rows, item.y + getItemHeight(item));
-    for (let i = item.letters.length; i-- > 0; ) {
-      let x: number;
-      let y: number;
-      if (item.vertical) {
-        x = item.x;
-        y = item.y + i;
-      } else {
-        x = item.x + i;
-        y = item.y;
-      }
-
-      let index = cells.findIndex((c) => c.x === x && c.y === y);
-      if (index < 0) {
-        index = cells.length;
-        cells.push({ x, y, value: item.letters[i]});
-      }
-      if (item.vertical) {
-        cells[index].hasTop = i > 0;
-        cells[index].hasBottom = i < item.letters.length - 1;
-      } else {
-        cells[index].hasLeft = i > 0;
-        cells[index].hasRight = i < item.letters.length - 1;
-      }
-    }
-  }
-
-  // Hide intersections:
-  for (const cell of cells) {
-    cell.isHidden = (cell.hasBottom || cell.hasTop) && (cell.hasLeft || cell.hasRight);
-  }
-
-  return  { cols, rows, cells, items };
-}
-
-function makeFrame(x: number, y: number, width: number, height: number) {
-  return `M${x} ${y}v${height}h${width}v${-height}z`;
-}
-
-function makePath(cells: ReadonlyArray<CWCell>): string {
-  const lines = getLines(cells);
-  // console.log('Line count:', lines.length);
-  mergeLines(lines);
-  // console.log('Shape count:', lines.length);
-  let path = '';
-  for (const shape of lines) {
-    path += 'M' + shape.join(' ') + (shape[0] === shape[shape.length - 1] ? 'z' : ' ');
-  }
-  // console.log(path);
-  return path;
-}
-
-function getLines(cells: ReadonlyArray<CWCell>) {
-  const buf = [] as string[][];
-
-  for (const cell of cells) {
-    if (!cell.hasTop) {
-      buf.push([`${cell.x},${cell.y}`, `${cell.x + 1},${cell.y}`]);
-    }
-    if (!cell.hasRight) {
-      buf.push([`${cell.x + 1},${cell.y}`, `${cell.x + 1},${cell.y + 1}`]);
-    }
-    if (!cell.hasBottom) {
-      buf.push([`${cell.x + 1},${cell.y + 1}`, `${cell.x},${cell.y + 1}`]);
-    }
-    if (!cell.hasLeft) {
-      buf.push([`${cell.x},${cell.y + 1}`, `${cell.x},${cell.y}`]);
-    }
-  }
-  return buf;
-}
-
-function joinLines(a: string[], b: string[]): string[] | undefined {
-  return a[a.length - 1] === b[0] ? a.concat(b.slice(1))
-    : b[b.length - 1] === a[0] ? b.concat(a.slice(1))
-    : undefined;
-}
-
-function mergeLines(lines: string[][]) {
-  for (let i = lines.length; i-- > 1;) {
-    const a = lines[i];
-    for (let j = i; j-- > 0;) {
-      const b = lines[j];
-      const c = joinLines(a, b);
-      if (c) {
-        lines[j] = c;
-        lines.splice(i, 1);
-        // Restart merging:
-        i = lines.length;
-        break;
-      }
-    }
-  }
-}
-
-type CWCell = Point & {
-  value: string;
-  isHidden?: boolean;
-  // neighbours, if any
-  hasLeft?: boolean;
-  hasRight?: boolean;
-  hasTop?: boolean;
-  hasBottom?: boolean;
-};
+// function disinfect(resourceName: any): string {
+//   const RE_FORBIDDEN_CHARS = /[^a-z\-_\.0-9]+/gmi;
+//   return ('' + resourceName).replace(RE_FORBIDDEN_CHARS, '');
+// }
 
 @Component({
   selector: 'app-crossword',
@@ -142,11 +38,15 @@ type CWCell = Point & {
   styleUrls: ['./crossword.component.scss']
 })
 export class CrosswordComponent implements OnInit {
-  game = createGame(parse(TEST));
-  pathFrame = makeFrame(-1, -1, this.game.cols + 2, this.game.rows + 2) + makePath(this.game.cells);
+  boardState: CellState[];
+  data = parse(TEST);
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onBoardChange(state: CellState[]) {
+    this.boardState = state.filter((cell) => cell.isActive && cell.value !== cell.answer);
   }
 }
