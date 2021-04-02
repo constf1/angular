b7052e6b54a1bb8b937e3b04a0a25902daa5f9c2
mico <mi-const@yandex.ru>
2021-04-02 19:17:11 +0300

    crossword: create dialog prototype stable

--- src/app/school/crossword/crossword-sidenav/crossword-sidenav.component.ts
+++ src/app/school/crossword/crossword-sidenav/crossword-sidenav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { CrosswordCreateDialogComponent } from '../crossword-create-dialog/crossword-create-dialog.component';
+import { CWTerm } from '../crossword-game/crossword-game.component';
 import { CrosswordSettingsService } from '../services/crossword-settings.service';
 
 @Component({
@@ -9,6 +10,7 @@ import { CrosswordSettingsService } from '../services/crossword-settings.service
   styleUrls: ['./crossword-sidenav.component.scss']
 })
 export class CrosswordSidenavComponent implements OnInit {
+  items: ReadonlyArray<CWTerm>;
 
   get mode() {
     return this.settings.state.sidenavModeSide ? 'side' : 'over';
@@ -28,7 +30,12 @@ export class CrosswordSidenavComponent implements OnInit {
   }
 
   openCreateDialog() {
-    this.dialog.open(CrosswordCreateDialogComponent);
+    const dialogRef = this.dialog.open(CrosswordCreateDialogComponent);
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.items = result;
+      }
+    });
   }
 
 }
