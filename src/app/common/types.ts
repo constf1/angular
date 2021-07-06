4febf4b38b43045ff12544a701bc5d3fcd02a09c
mico <mi-const@yandex.ru>
2021-07-06 14:16:45 +0300

    common: + ReadonlyByKey

--- src/app/common/types.ts
+++ src/app/common/types.ts
@@ -12,3 +12,10 @@ export type SubType<A, T> = Pick<A, { [K in keyof A]: A[K] extends T ? K : never
  * ignores `readonly` properties
  */
 export type Writeable<T> = { -readonly [P in keyof T]-?: T[P] };
+
+export type KeyOfType<T, V> = {
+  [P in keyof T]: T[P] extends V ? P: never
+} [keyof T];
+
+export type ReadonlyByKey<T extends {} , K extends keyof T> = Readonly<Pick<T, K>> & Omit<T, K>;
+export type ReadonlyByType<T, V> = ReadonlyByKey<T, KeyOfType<T, V>>;
