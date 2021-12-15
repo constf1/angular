98833f68435bb2ddd0b3dea350b63b4c32c79243
mico <mi-const@yandex.ru>
2021-12-15 12:05:42 +0300

    index selector small refactoring

--- src/app/material/index-selector/index-selector.component.ts
+++ src/app/material/index-selector/index-selector.component.ts
@@ -1,23 +1,28 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-index-selector',
   templateUrl: './index-selector.component.html',
   styleUrls: ['./index-selector.component.scss']
 })
-export class IndexSelectorComponent implements OnInit {
+export class IndexSelectorComponent {
+  /** The value to be used for display purposes. */
   @Input() label = 'Index';
-  @Input() count = 0;
+  /** The minimum value that the selector can have. */
+  @Input() min = 0;
+  /** The maximum value that the selector can have. */
+  @Input() max = -1;
+
   @Input() selection = -1;
   @Output() selectionChange = new EventEmitter<number>();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  /** Whether the component is disabled. */
+  get disabled(): boolean {
+    return this.max < this.min;
   }
 
   requestSelectionChange(value: number) {
-    if (value !== this.selection && value >= 0 && value < this.count) {
+    if (value !== this.selection && value >= this.min && value <= this.max) {
       this.selectionChange.emit(value);
     }
   }
